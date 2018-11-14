6bc05f894c692c658881c0f9089dd61f36039ab1
sisi <baba329@qq.com>
2018-11-14 21:26:35 +0800

    第一次上传

---
+++ js/checkLogin.js
@@ -0,0 +1,20 @@
+/*检查登录状态*/
+var xhr3 = new XMLHttpRequest();
+xhr3.onreadystatechange = function (ev) {
+    if (xhr3.readyState === 4 && xhr3.status === 200) {
+        var resp = JSON.parse(xhr3.responseText);
+        var login_status = document.getElementById("login_status");
+        if (resp.code === 1) {
+
+            login_status.innerText = "注销";
+        }
+        else {
+            login_status.innerHTML = "登录";
+
+        }
+    }
+};
+
+xhr3.open("post", "http://47.99.87.23/api/user/isSignIn");
+//xhr3.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+xhr3.send();
