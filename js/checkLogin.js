cdd351db5c61f85135e6ebc709f5e5da0bad38ed
sisi <baba329@qq.com>
2018-11-17 16:11:02 +0800

    no message

--- js/checkLogin.js
+++ js/checkLogin.js
@@ -1,5 +1,6 @@
 /*检查登录状态*/
 var xhr3 = new XMLHttpRequest();
+xhr3.withCredentials = true;
 xhr3.onreadystatechange = function (ev) {
     if (xhr3.readyState === 4 && xhr3.status === 200) {
         var resp = JSON.parse(xhr3.responseText);
